33871cee1ac201be65cd6151d9d7c1413eb6f96d
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2026-05-02 09:47:25 +0800

    Fix composite window offsets, markup preview alignment, and template config persistence

--- ok/gui/tasks/TemplateTab.py
+++ ok/gui/tasks/TemplateTab.py
@@ -359,7 +359,7 @@ class TemplateTab(QWidget):
     def __init__(self, config: Config):
         super().__init__()
         self.setObjectName("TemplateTab")
-        self.template_tab_config = config.get('template_tab', {
+        self.template_tab_config = Config('template_tab', {
             'generate_label_enum': False,
             'label_enum_relative_path': 'ok_tasks/LabelEnum.py'
         })
